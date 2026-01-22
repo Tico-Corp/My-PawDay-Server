b9cafc80e691a3b26f60ce880baad353b2838bc1
River is line <dmstjs7047@hanmail.net>
2026-01-22 11:16:34 +0900

    init : init project

---
+++ mypawday/src/main/java/com/tico/mypawday/MypawdayApplication.java
@@ -0,0 +1,13 @@
+package com.tico.mypawday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MypawdayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MypawdayApplication.class, args);
+	}
+
+}
---
+++ mypawday/src/test/java/com/tico/mypawday/MypawdayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tico.mypawday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MypawdayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
